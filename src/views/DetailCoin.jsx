6952b8ac9e2d9296500386a2a295ec9bf9118f85
arie <arieinjector@gmail.com>
2022-05-04 09:34:05 +0700

    develop chart

--- src/views/DetailCoin.jsx
+++ src/views/DetailCoin.jsx
@@ -1,24 +1,30 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function DetailCoint() {
+    const [historiChart, setHistoriChart] = useState(null)
     const params = useParams()
     
-    const getDataChart = async () => {
-        await axios.get(`https://api.coingecko.com/api/v3/coins/${params.coin}?sparkline=true&localization=false&tickers=false&community_data=false&developer_data=false`)
-                    .then(response => console.log(response.data.market_data.sparkline_7d.price))
-    }
-
-    getDataChart()
     
-
+    useEffect(() => {
+        const getDataChart = async () => {
+            await axios.get(`https://api.coingecko.com/api/v3/coins/${params.coin}/market_chart?vs_currency=usd&days=365`)
+                        .then(response => {
+                            if (response.status === 200) {
+                                setHistoriChart(response.data.prices)
+                            }
+                        })
+        }
+        getDataChart()
+    }, [params])
+    
     return (
         <div>
             <p>Ini Detail Coin</p>
+            {console.log(historiChart)}
         </div>
     )
 }
 
 export default DetailCoint;
-
-// response.data.market_data.sparkline_7d.price
